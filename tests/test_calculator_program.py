56ee055f0cc2cdb4bb5ac1598f9c284e017aeb3c
kuznecanna13 <kuznecanna13@gmail.com>
2024-11-30 16:52:53 +1200

    second commit

--- tests/test_calculator_program.py
+++ tests/test_calculator_program.py
@@ -6,9 +6,18 @@ def test_calculate_addition():
 
 def test_calculate_division():
     assert calculate(8, 2, '/') == 4
+    assert calculate(1, 0, '/') == "Ошибка: Деление на ноль."
 
 def test_calculate_unknown_operation():
     assert calculate(5, 5, 'unknown') == "Неизвестная операция."
+
+def test_calculate_minus():
+    assert calculate(3, 2, '-') == 1
+
+def test_calculate_multiply():
+    assert calculate(2, 5, '*') == 10
+    assert calculate(0, 5, '*') == 0
+    assert calculate(0.2, 5, '*') == 1
 
 '''
 Задача. В настоящий момент реализовано три unit-теста
